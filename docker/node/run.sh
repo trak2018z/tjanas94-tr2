091865bddd547c74bed1baadd2cff1f9494201f6
Tomasz Janas <t.janas@iccomplex.eu>
2017-10-29 20:47:43 +0100

    poprawa konfiguracji dockera dla raspberry pi

--- docker/node/run.sh
+++ docker/node/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Creating node_modules and dist directories"
+mkdir -p dist
+chown -R node:node node_modules dist
+
 echo "Installing node modules"
-yarn
+su node sh -c "yarn"
 
 echo "Starting server"
-yarn start
+su node sh -c "yarn start"
